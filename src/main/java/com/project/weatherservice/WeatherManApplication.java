75b13130a00d7a1241e4ad449a4eed15e43aade3
aditya23k <23adikam@gmail.com>
2023-11-22 14:40:22 +0530

    Initialized Project

---
+++ src/main/java/com/project/weatherservice/WeatherManApplication.java
@@ -0,0 +1,13 @@
+package com.project.weatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherManApplication.class, args);
+	}
+
+}
