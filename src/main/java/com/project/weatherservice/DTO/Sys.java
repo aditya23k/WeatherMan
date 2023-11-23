63df9ca9e420c5dac4b8bb92840b2533e17b5449
aditya23k <23adikam@gmail.com>
2023-11-23 09:32:20 +0530

    removed duplicate code

--- src/main/java/com/project/weatherservice/DTO/Sys.java
+++ src/main/java/com/project/weatherservice/DTO/Sys.java
@@ -26,31 +26,3 @@ public class Sys {
 
 
 }
-package com.project.weatherservice.DTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class Sys {
-
-    private String type;
-    private String id;
-    private String message;
-    private String country;
-    private String sunrise;
-    private String sunset;
-
-    public Sys(String type, String id, String message, String country, String sunrise, String sunset) {
-        this.type = type;
-        this.id = id;
-        this.message = message;
-        this.country = country;
-        this.sunrise = sunrise;
-        this.sunset = sunset;
-    }
-
-
-
-}
